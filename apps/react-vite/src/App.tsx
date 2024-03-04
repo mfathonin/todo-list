a01a7cc0deb7007ddd69e9641cb55a3758be7a7e
Fathoni <mfathonin@gmail.com>
2024-03-04 22:49:51 +0700

    setup: Add react apps

---
+++ apps/react-vite/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Todo List</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/react-vite/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  return {
+    server: {
+      port: parseInt(`${env.VITE_PORT ?? 3000}`),
+    },
+    plugins: [react()],
+  };
+});
