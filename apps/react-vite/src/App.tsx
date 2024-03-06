d9c9c19591fe7a78623cbe004cae4a63b1527280
Fathoni <mfathonin@gmail.com>
2024-03-06 21:05:29 +0700

    feat: Implement component in to `react-vite`

--- apps/react-vite/src/App.tsx
+++ apps/react-vite/src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonWithIcon } from "components/styled-components";
+import { Container, Button, TextField } from "components";
 
 function App() {
   const handleClick = () => {
@@ -6,39 +6,15 @@ function App() {
   };
 
   return (
-    <>
+    <Container>
       <h1>Todo List</h1>
-      <Button onClick={handleClick}>Add</Button>
-      <ButtonWithIcon
-        onClick={handleClick}
-        icon={
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 5V19"
-              stroke="#a5d8ff"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M5 12H19"
-              stroke="#a5d8ff"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        }
-      >
-        Add
-      </ButtonWithIcon>
-    </>
+      <form>
+        <TextField placeholder="type here create todo" />
+        <Button type="submit" onClick={handleClick}>
+          Add
+        </Button>
+      </form>
+    </Container>
   );
 }
 
